f37260bfbc16fbffae1f99f4d992b0ad7e34cd87
Roy Alameh <roy.alameh24@d155.org>
2023-01-23 08:57:46 -0600

    Initial commit

--- mapNotes/ViewController.swift
+++ mapNotes/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var currentLocation : CLLocation?
+    @IBOutlet weak var mapOutlet: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapOutlet.showsUserLocation = true
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+    }
+    
+    
+    @IBAction func zoomAction(_ sender: UIBarButtonItem) {
+        print("test")
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation?.coordinate
+        let region = MKCoordinateRegion(center: center!, span: coordinateSpan)
+        mapOutlet.setRegion(region, animated: true)
+    }
+    
 }
 
